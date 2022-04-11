b6de99676d7f43ed58c1f098ab680355af7fc60b
Zainab Oyedeji <ololadeoyedejiii@gmail.com>
2022-04-11 12:39:03 +0100

    initial commit

---
+++ src/Components/AppNav/appnav.jsx
@@ -0,0 +1,19 @@
+import stripe from "../../img/stripe.png";
+import React from "react";
+import "./appnav.scss";
+
+function AppNav() {
+    return (
+      <div className="appnav">
+        <div className="logo">
+          <img src={stripe} alt="#"/>
+          </div>
+          <div>
+            
+          </div>
+      </div>
+    );
+  }
+  
+  export default AppNav;
+  
---
+++ src/Pages/LandingPage/landingpage.jsx
@@ -0,0 +1,11 @@
+import AppNav from "../../Components/AppNav/appnav";
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <AppNav/>
+    </div>
+  );
+}
+
+export default LandingPage;
